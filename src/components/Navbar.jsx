d91b3d1109a7c0b7542296e01bd1ddc84c7eb794
yeah-mr-white <eyeforparrot78145@gmail.com>
2024-07-29 23:15:36 +0700

    added dropdown toggle button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+// magic code 0
+import React, { useState } from "react";
 import logo from "../assets/vector/logo.svg";
 import hamburger from "../assets/vector/hamburger.svg";
+import cross from "../assets/vector/cross.svg";
 
 const Navbar = () => {
+    // magic code 1
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
     return (
-        <nav className="shadow-borderColor-100/25 fixed left-0 top-0 z-10 w-full border-b border-borderColor/10 bg-customizedBg/50 p-4 shadow-lg backdrop-blur-xl sm:px-0 sm:py-4">
+        <nav className="shadow-borderColor-100/25 fixed left-0 top-0 z-10 w-full border-b border-borderColor/10 bg-customizedBg/50 p-4 shadow-lg backdrop-blur-xl sm:px-0">
             <div className="max-container mx-auto flex justify-between font-firaCode text-slate-100">
                 {/* start - logo */}
                 <div className="flex py-1 align-middle">
@@ -15,7 +25,6 @@ const Navbar = () => {
                         shinnthant.dev
                     </a>
                 </div>
-
                 {/* middle - nav links */}
                 <div className="hidden py-1.5 align-middle sm:inline-block">
                     <div className="flex gap-12">
@@ -26,14 +35,58 @@ const Navbar = () => {
                         <div>#contact-me</div>
                     </div>
                 </div>
-
                 {/* end -> button */}
                 <div className="flex items-center justify-center sm:hidden">
-                    <img src={hamburger} alt="Hamburger" className="w-6" />
+                    <button onClick={toggleDropdown} className="w-4">
+                        {/* magic code 2 */}
+                        <img
+                            src={isDropdownOpen ? cross : hamburger}
+                            alt="Hamburger & cross icons"
+                            className="w-6"
+                        />
+                    </button>
                 </div>
                 <button className="hidden h-10 rounded-lg bg-customizedBtnBg px-3.5 py-2.5 text-sm text-slate-950 lg:block">
                     download-cv
                 </button>
+                {/* dropdown menu for mobile */}
+                {/* magic code 3 */}
+                {isDropdownOpen && (
+                    <div className="absolute left-0 top-full w-full sm:hidden">
+                        <div className="flex flex-wrap">
+                            <a
+                                href="#"
+                                className="w-full border-b border-borderColor bg-customizedBg py-4 pl-4"
+                            >
+                                #home
+                            </a>
+                            <a
+                                href="#"
+                                className="w-full border-b border-borderColor bg-customizedBg py-4 pl-4"
+                            >
+                                #projects
+                            </a>
+                            <a
+                                href="#"
+                                className="w-full border-b border-borderColor bg-customizedBg py-4 pl-4"
+                            >
+                                #about-me
+                            </a>
+                            <a
+                                href="#"
+                                className="w-full border-b border-borderColor bg-customizedBg py-4 pl-4"
+                            >
+                                #skills
+                            </a>
+                            <a
+                                href="#"
+                                className="w-full border-b border-borderColor bg-customizedBg py-4 pl-4"
+                            >
+                                #contact-me
+                            </a>
+                        </div>
+                    </div>
+                )}
             </div>
         </nav>
     );
